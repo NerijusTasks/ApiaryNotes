fbda817402c9a832031497626b893ea2d0062e5b
Nerijus <Nerijus@DESKTOP-08FUU9C>
2023-11-25 22:16:16 +0200

    Add project files.

---
+++ ApiaryNotes.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiaryNotes.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
