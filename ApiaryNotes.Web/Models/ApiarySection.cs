a1aa3c3aec40e7bcec47aaedb36216d651c8385f
Nerijus <Nerijus@DESKTOP-08FUU9C>
2023-11-26 09:04:13 +0200

    Create models

---
+++ ApiaryNotes.Web/Models/ApiarySection.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiaryNotes.Web.Models
+{
+    public class ApiarySection
+    {
+        public Guid ApiaryId { get; set; }
+        public string Name  { get; set; }
+    }
+}
---
+++ ApiaryNotes.Web/Models/Notes/Note.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiaryNotes.Web.Models.Notes
+{
+    public class Note
+    {
+        public Guid NoteId { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+
+        [Required(ErrorMessage = "Įrašykite tekstą")]
+        public string? Text { get; set; }
+        public Guid ApiarySectionId { get; set; }
+        public ApiarySection ApiarySections { get; set; }
+    }
+}
